15cb6c126edcfac0727070a4c364cc08e3a8611d
ThamerAyachi <thamerjak3@gmail.com>
2024-05-21 21:00:01 +0100

    add list offers in "entreprise" application

--- entreprise/src/services/offer-service.js
+++ entreprise/src/services/offer-service.js
@@ -6,6 +6,19 @@ import authHeader from './data-service';
 export class OfferService {
   constructor() {}
 
+  async getOffersPublic(limit = 5, page = 1, q = '') {
+    try {
+      const config = { headers: { ...authHeader() } };
+      const { data } = await axios.get(
+        `${API_URL}/offer?limit=${limit}&page=${page}&q=${q}`,
+        config
+      );
+      return data;
+    } catch (e) {
+      throw new Error(e.response.data.error);
+    }
+  }
+
   async getOffers() {
     try {
       const config = { headers: { ...authHeader() } };
